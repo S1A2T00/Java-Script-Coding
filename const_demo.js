bd78a22318dea3b440a25dcb87c832d964a82589
Satyam Pandey <sp6540788@gmail.com>
2026-02-06 18:28:04 +0530

    Add files via upload

---
+++ const_demo.js
@@ -0,0 +1,9 @@
+const pi=3.14;
+ pi=3.15; 
+ console.log(pi);
+ 
+ const arr=[1,2,"Hello"];
+    arr.push(3);
+    console.log("Array after push:",arr);
+    arr=[4,5,6]; 
+    console.log("Array after reassignment:",arr);
