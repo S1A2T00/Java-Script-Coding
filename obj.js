d9ef60a88330e43cf40879b0b8aac739a4cf7a0f
Satyam Pandey <sp6540788@gmail.com>
2026-02-06 18:31:30 +0530

    Add files via upload

---
+++ obj.js
@@ -0,0 +1,28 @@
+const user = {
+    name: "John",
+    age: 30,
+    address: {
+        street: "123 Main St",
+        city: "New York",
+        zip: "10001"
+    },
+    contact: {
+        email: "john@example.com",
+        phone: "9559716639"
+    }
+};
+
+//console.log(user.address.city); 
+//console.log(user.contact.email);
+//console.log(user.contact.phone);
+
+
+
+
+
+
+
+let x=[1,2,3];
+let y=x;
+y[0]=10;
+console.log(x[0]);
