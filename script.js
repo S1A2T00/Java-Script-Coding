8801bffdacbfd778d2f8e78ef480cba5e0747444
Satyam Pandey <sp6540788@gmail.com>
2026-02-06 18:28:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log(myVar);
+var myVar=10;
+console.log(myVar);
+var myVar=20;
+console.log(myVar);
+
+function demoFunction(){
+    console.log(innerVar);
+    var innerVar=30;
+    console.log(innerVar);
+}
+demoFunction();
