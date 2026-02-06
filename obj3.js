66e6ad96cd3579e0830bc98845616547c0865a78
Satyam Pandey <sp6540788@gmail.com>
2026-02-06 18:31:52 +0530

    Add files via upload

---
+++ obj3.js
@@ -0,0 +1,12 @@
+class person{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+}
+let p1=new person("Satyam",20);
+console.log(p1);
+let p2=new person("Rahul",22);
+console.log(p2);
+p1==p2;
+console.log(p1==p2);
