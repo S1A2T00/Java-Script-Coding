ab6a60035bc65b9c42e7c6e0fd338fab67cd8248
Satyam Pandey <sp6540788@gmail.com>
2026-02-06 18:28:35 +0530

    Add files via upload

---
+++ let_demo.js
@@ -0,0 +1,31 @@
+let myLet=10;
+console.log(myLet);
+ myLet=20;
+ {
+    let x=50;
+    console.log(x);
+ }
+ {
+    let x=100;
+    console.log(x);
+ }
+ {
+    let x=50;
+    console.log(x);
+    {
+        let x=100;
+        let y=200;
+        console.log(y);
+        console.log(x);
+    }
+ }
+ {
+    let x=50;
+    console.log(x);
+    if(true)
+    {
+        let x=100;
+        console.log(x);
+    }
+    console.log(x);
+ }
