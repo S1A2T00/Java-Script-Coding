40047a3b99b0aedf60c54896b54f40245711d827
Satyam Pandey <sp6540788@gmail.com>
2026-02-06 18:29:39 +0530

    Add files via upload

---
+++ arrowFunction_demo.js
@@ -0,0 +1,18 @@
+let f1=() => { 
+    console.log("Hello from arrow function");
+};
+
+
+let mul=function(a,b){
+    return a*b;
+};
+console.log(mul(3,4));
+let divide=function dividenum(a,b){
+    return a/b;
+};
+console.log(divide(10,2));
+
+
+
+let sub=(a,b) => {return a-b;};
+console.log(sub(10,4));
